035ea0ee173c98b121f7d9fe617e187a84d3ba4c
Prateek Kumar <singhprateek474@gmail.com>
2023-09-06 17:55:36 +0530

    Code Clean Up

--- Frontend/src/header/sidenav.js
+++ Frontend/src/header/sidenav.js
@@ -5,8 +5,6 @@ export default function SideNav(){
       
     let navtoggle = document.querySelector(".nav-toggle").childNodes;
     let navlist = document.querySelector(".nav-list");
-    let dropdown = document.querySelectorAll(".dropdown");
-    let navlistitems = document.querySelectorAll(".nav-list ul li a");
 
     navtoggle[0].addEventListener('click',(e)=>{
       navtoggle[0].style.display = 'none';
@@ -19,18 +17,6 @@ export default function SideNav(){
       navtoggle[0].style.display = 'block';
       navlist.style.transform = 'translateX(-100%)';
     });
-
-    for(let i=0;i<navlistitems.length;i++){
-      navlistitems[i].addEventListener('click',()=>{
-        if(window.getComputedStyle(dropdown[i]).display == 'none'){
-          dropdown[i].style.display = 'block';
-          for(let j=0;j<dropdown.length;j++){
-            if(i != j) dropdown[j].style.display = 'none';
-          }
-        }   
-        else dropdown[i].style.display = 'none';
-      });
-    }
     
   });
 
